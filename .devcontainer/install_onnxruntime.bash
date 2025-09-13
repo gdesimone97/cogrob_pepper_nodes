db5b53478e322696341dcd03f9379e97714cc3c0
Giuseppe De Simone <gds2011@libero.it>
2025-09-13 12:30:10 +0200

    transformers & onnx

--- .devcontainer/install_onnxruntime.bash
+++ .devcontainer/install_onnxruntime.bash
@@ -4,10 +4,10 @@ ARCH=$(uname -m)
 
 if [[ "$ARCH" == "x86_64" ]]; then
     echo "Installing onnxruntime-gpu"
-    pip install onnxruntime-gpu
+    pip install onnxruntime-gpu==1.22.0
 elif [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
     echo "Installing onnxruntime"
-    pip install onnxruntime
+    pip install onnxruntime==1.22.0
 else
     echo "Unsupported architecture: $ARCH"
     exit 1
