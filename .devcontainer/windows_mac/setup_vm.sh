feef02f1d4c93161adf8cfc49d1271c7b3d1af29
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2025-09-17 11:18:59 +0200

    dialout

--- .devcontainer/windows_mac/setup_vm.sh
+++ .devcontainer/windows_mac/setup_vm.sh
@@ -91,3 +91,12 @@ export ROS_DOMAIN_ID=0
 export PYTHONPATH=/workspace/demo_utils:$PYTHONPATH
 
 EOF
+
+usermod -aG dialout mivia
+cat <<EOF >> /etc/udev/rules.d/99-usb-dialout.rules
+
+SUBSYSTEM=="usb", ATTR{idVendor}=="2886", ATTR{idProduct}=="0018", GROUP="dialout", MODE="0660"
+
+EOF
+
+reboot
