d5f80f3d6e3eba921454cd34a2a37c12da4d45fe
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2025-09-17 12:01:22 +0200

    update conf

--- .devcontainer/windows_mac/setup_vm.sh
+++ .devcontainer/windows_mac/setup_vm.sh
@@ -92,6 +92,11 @@ export PYTHONPATH=/workspace/demo_utils:$PYTHONPATH
 
 EOF
 
-source configure_dev.bash
+usermod -aG dialout mivia
+cat <<EOF >> /etc/udev/rules.d/99-usb-dialout.rules
+
+SUBSYSTEM=="usb", ATTR{idVendor}=="2886", ATTR{idProduct}=="0018", GROUP="dialout", MODE="0660"
+
+EOF
 
 reboot
